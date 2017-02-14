1bd292af5d6e33bd9ce6d554483c4ec62946e449
Matt Pedersen <mattpp@Matts-MacBook-Air.local>
2017-02-14 11:53:04 -0800

    begin to write frontend js

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,1 +1,39 @@
-// Write your Javascript code.
+// feed, play, work, sleep
+var $feedBtn  = $('.feed-btn'),
+    $playBtn  = $('.play-btn'),
+    $workBtn  = $('.work-btn'),
+    $sleepBtn = $('.sleep-btn')
+
+
+function updateDachi(act, callback) {
+    var url;
+    if (act == 'feed') url = '/feed'
+    else if (act == 'play') url = '/play'
+    else if (act == 'work') url = '/work'
+    else if (act == 'sleep') url = '/sleep'
+    else callback('Something went wrong', null)
+
+    $.post(url, function(data) {
+        console.log(data, url)
+    })
+}
+
+
+$feedBtn.on('click', function(e) {
+    updateDachi('feed', function(err, data) {
+        if (err) console.log(err)
+        console.log('inside cb')
+    })
+})
+
+$playBtn.on('click', function(e) {
+    console.log('hello werld!!! playbtn')
+})
+
+$workBtn.on('click', function(e) {
+    console.log('hello werld!!! workbtn')
+})
+
+$sleepBtn.on('click', function(e) {
+    console.log('hello werld!!! sleepbtn')
+})
